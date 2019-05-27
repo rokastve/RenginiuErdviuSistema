32eee6ed4cd0791d9a0ba19ebe7e9e8340263ce3
evaale <44762048+evaale@users.noreply.github.com>
2019-05-27 08:39:16 +0300

    FoodSupplierController

---
+++ Projektas/Projektas/Controllers/FoodSupplierController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Projektas.Models;
+
+namespace Projektas.Controllers
+{
+    public class FoodSupplierController : Controller
+    {
+        // GET: FoodSupplier
+        public ActionResult FoodSupplierList()
+        {
+            List<FoodSupplier> foodSupplierList = new List<FoodSupplier>();
+            using (DBEntities db = new DBEntities())
+            {
+                foodSupplierList = db.FoodSupplier.ToList<FoodSupplier>();
+            }
+            return View(foodSupplierList);
+        }
+    }
+}
