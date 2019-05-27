c153ce85ccd4977977339437bff37276164ce8f0
kamilest <kamile.stugyte@ktu.edu>
2019-05-27 09:32:05 +0300

    User Order

---
+++ Projektas/Projektas/Controllers/OrderController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Projektas.Models;
+
+namespace Projektas.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult OrderListView()
+        {
+            List<UserOrder> OrderList = new List<UserOrder>();
+            using (DBEntities db = new DBEntities())
+            {
+                OrderList = db.UserOrder.ToList<UserOrder>();
+            }
+            return View(OrderList);
+        }
+
+        // GET: Order/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Order/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Order/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Order/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
