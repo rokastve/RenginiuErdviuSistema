52c31f3e4acaa6a66d6cdb7dedcd1634571a1e0a
Rokas Tverijonas <rokas.tverijonas@ktu.edu>
2019-05-27 03:01:56 +0300

    Initial Commit

---
+++ Projektas/Projektas/Controllers/RegisteredUserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Projektas.Models;
+namespace Projektas.Controllers
+{
+    public class RegisteredUserController : Controller
+    {
+        // GET: RegisteredUser
+        public ActionResult UserList()
+        {
+            List<RegisteredUser> userList = new List<RegisteredUser>();
+            using (DBEntities db = new DBEntities())
+            {
+                userList = db.RegisteredUser.ToList<RegisteredUser>();
+            }
+                return View(userList);
+        }
+    }
+}
---
+++ Projektas/Projektas/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Projektas.MvcApplication" Language="C#" %>
