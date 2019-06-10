3728b04311ccadee6683d25a34c850ead26f49a8
Rokas Tverijonas <rokas.tverijonas@ktu.edu>
2019-06-10 05:24:26 +0300

    Changes to equipment adding

--- Projektas/Projektas/Controllers/EquipmentOrderController.cs
+++ Projektas/Projektas/Controllers/EquipmentOrderController.cs
@@ -17,21 +17,23 @@ namespace Projektas.Controllers
 
         public ActionResult Add(int id)
         {
+            int idret = id;
             List<Equipment> things = (List<Equipment>)TempData["Things"];
             foreach (Equipment eq in things)
             {
-                UserOrder UO = new UserOrder();
+                int code = 0;
+                
                 List<UserOrder> UserOrderList = new List<UserOrder>();
                 using (DBEntities db = new DBEntities())
                 {
                     UserOrderList = db.UserOrder.ToList<UserOrder>();
 
                     if (UserOrderList.Count == 0)
-                        UO.Order_code = 0;
+                        code = 0;
 
                     if (UserOrderList.Count > 0)
-                        UO.Order_code = UserOrderList.Max(x => x.Order_code) + 1;
-                    UO.Reservation = id;
+                        code = UserOrderList.Max(x => x.Order_code) + 1;
+                    UserOrder UO = new UserOrder(code, idret);
                     db.UserOrder.Add(UO);
                     db.SaveChanges();
                     EquipmentOrder order = new EquipmentOrder(eq.Code, UO.Order_code);
@@ -39,7 +41,7 @@ namespace Projektas.Controllers
                     db.SaveChanges();
                 }
             }
-            return null;
+            return RedirectToAction("Details", "Reservation", new { id = idret });
         }
 
         // GET: EquimentOrder/Details/5
